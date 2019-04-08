02df06df1ef6ea1edf2b7102fd8696d26762a4d2
jorgegarciaruiz <45965355+jorgegarciaruiz@users.noreply.github.com>
2019-04-08 19:30:01 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,6 +1,24 @@
 $(function () {
    $("#creditos").on("click",function () {
        $("#creditos h2").css("padding-bottom",0);
+       $("#creditos h2:hover").css("background-color","rgba(66,66,66,0.95)");
+       $("#creditos h2:hover").css("color","white");
        $("#js").html("Adrián Calvo"+"<br>"+"Jorge García");
    });
 });
+$(function () {
+    $("#desplegar").on("click",function () {
+
+        if ($("#menu").hasClass("quitar")) {
+            $("#portada").css("left", "17%");
+            $("#portada").css("width", "83%");
+            $("#menu").removeClass("quitar");
+            $(".conten").addClass("contenjs");
+        }else{
+            $("#portada").css("left", "");
+            $("#portada").css("width", "100%");
+            $("#menu").addClass("quitar");
+            $(".conten").removeClass("contenjs");
+        }
+    });
+})
